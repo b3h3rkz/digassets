6ebd3ae5f06d8eaa41e3744b895b11532e596308
eddyamewu <eddyamewu@gmail.com>
2018-03-13 10:32:47 +0000

    blahh

--- app/controllers/CoinListController.js
+++ app/controllers/CoinListController.js
@@ -1,9 +1,4 @@
 angular.module('BitnobApp').controller('CoinListController', function () {
     var vm = this;
-
-    vm.alert = function(){
-        alert("Hello, Rele");
-    };
-
-    vm.alert();
+    
 });
